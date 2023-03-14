2a176cd464445ef8b5147d81f9e5a16d23150d10
Jesse <jlarson@iworq.com>
2023-03-14 15:15:48 -0600

    endpoints with different methods

--- server/index.js
+++ server/index.js
@@ -23,7 +23,13 @@ server.on('request', (req, res) => {
     const items = req.url.split('/');
     // /friends/2 => ['', 'friends', '2']
 
-    if (items[1] === 'friends') {
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+        })
+    } else if (req.method === 'GET' && items[1] === 'friends') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         if (items.length === 3) {
@@ -33,7 +39,7 @@ server.on('request', (req, res) => {
             res.end(JSON.stringify(friends));
         }
         res.end();
-    } else if (items[1] === 'messages') {
+    } else if (req.method === 'GET' && items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
