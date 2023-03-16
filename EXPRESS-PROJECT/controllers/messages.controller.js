e61d6a012cec68797b87651a4586cedcd0603e57
jelarson <jesslarson93@gmail.com>
2023-03-15 21:47:38 -0600

    public folder

--- EXPRESS-PROJECT/controllers/messages.controller.js
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 function getMessages(req, res) {
-    res.send('<ul><li>Hello Albert!</li></ul>');
+    // res.send('<ul><li>Hello Albert!</li></ul>');
+    res.sendFile(path.join(__dirname, '..', 'public', 'rocket.png'));
 }
 
 function postMessage(req, res) {
